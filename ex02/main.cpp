fa431045361b47ec5bd991403ddcd4bafc1aec82
jstuhrin <codam.2y78l@simplelogin.com>
2026-04-15 10:50:00 +0200

    added tests to main.cpp

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -1,12 +1,49 @@
 
 #include "Bureaucrat.hpp"
 #include "AForm.hpp"
+#include "PresidentialPardonForm.hpp"
+#include "RobotomyRequestForm.hpp"
 #include "ShrubberyCreationForm.hpp"
 
 int main()
 {
   {
+    Bureaucrat arthur("arthur", 1);
+    std::cout << arthur;
+    PresidentialPardonForm presidentialPardonForm("jan");
+    arthur.signForm(presidentialPardonForm);
+    arthur.executeForm(presidentialPardonForm);
+  }
+  {
+    Bureaucrat arthur("arthur", 150);
+    std::cout << arthur;
+    PresidentialPardonForm presidentialPardonForm("jan");
+    arthur.signForm(presidentialPardonForm);
+    arthur.executeForm(presidentialPardonForm);
+  }
+  {
+    Bureaucrat arthur("arthur", 1);
+    std::cout << arthur;
+    RobotomyRequestForm robotomyRequestForm("jan");
+    arthur.signForm(robotomyRequestForm);
+    arthur.executeForm(robotomyRequestForm);
+  }
+  {
+    Bureaucrat arthur("arthur", 150);
+    std::cout << arthur;
+    RobotomyRequestForm robotomyRequestForm("jan");
+    arthur.signForm(robotomyRequestForm);
+    arthur.executeForm(robotomyRequestForm);
+  }
+  {
     Bureaucrat arthur("arthur", 42);
+    std::cout << arthur;
+    ShrubberyCreationForm shrubberyCreationForm("home");
+    arthur.signForm(shrubberyCreationForm);
+    arthur.executeForm(shrubberyCreationForm);
+  }
+  {
+    Bureaucrat arthur("arthur", 150);
     std::cout << arthur;
     ShrubberyCreationForm shrubberyCreationForm("home");
     arthur.signForm(shrubberyCreationForm);
