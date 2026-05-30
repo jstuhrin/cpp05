29f7f7cfff37211ed3ee466fe7999b1427fb9fb2
Jan Stuhring <jan.stuehring.wmp1u@simplelogin.com>
2026-05-30 10:55:01 +0200

    cleanup, added some comments

--- ex03/Intern.hpp
+++ ex03/Intern.hpp
@@ -6,12 +6,14 @@
 class Intern
 {
   public:
+    // OCF is pointless, since the Intern class does not manage raw resources, but it is required 
     Intern();
     Intern(const Intern& other);
     Intern& operator=(const Intern& other);
     ~Intern();
 
     AForm* makeForm(const std::string& formName, const std::string& target);
+
   private:
     AForm* makePresidentialPardonForm(const std::string& target);
     AForm* makeRobotomyRequestForm(const std::string& target);
